1e967e1cce14d2e411425e942df186fa7c321167
Bruno Girome <brunogirome@gmail.com>
2023-03-06 01:22:00 -0300

    library changes

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,26 @@ import { Server } from 'socket.io';
 
 import { keyboard } from '@nut-tree/nut-js';
 
+interface TelemetryInfo {
+  truckSpeed: number;
+  currentFuel: number;
+  totalFuel: number;
+
+  hazardLight?: boolean;
+  cruizeControl: boolean;
+  highLight: boolean;
+  engineBreak: boolean;
+
+  differential?: boolean;
+  light: boolean;
+  retarderStatus: number;
+
+  currentMaxSpeed: number;
+}
+
 keyboard.config.autoDelayMs = 0;
+
+const MIN_SPEED = 10;
 
 const telemetry = tst();
 
@@ -35,16 +54,52 @@ io.on('connection', socket => {
   });
 });
 
-telemetry.watch({ interval: 25 }, ({ truck }) => {
-  io.emit('speed_monitoring', truck.speed.kph);
+let lastSpeedLimit = MIN_SPEED;
 
-  io.emit('status_hazard_light', truck.lights.hazard?.enabled);
+telemetry.watch({ interval: 50 });
 
-  io.emit('differential_status', truck.differential.lock?.enabled);
+telemetry.truck.on('hazard', () => {
+  console.log('hazard');
+
+  io.emit('hazard_light', telemetry.getTruck().lights.hazard?.enabled || false);
 });
 
+telemetry.truck.on('speed', () => {
+  io.emit('speed', telemetry.getTruck().speed.kph);
+});
+
+// telemetry.watch({ interval: 25 }, ({ truck, navigation }) => {
+//   const currentSpeedLimit = navigation.speedLimit.kph;
+
+//   lastSpeedLimit = currentSpeedLimit >= 30 ? currentSpeedLimit : lastSpeedLimit;
+
+//   const speedLimit =
+//     currentSpeedLimit >= 30 ? currentSpeedLimit : lastSpeedLimit;
+
+//   // const apiTelemetry: TelemetryInfo = {
+//   //   truckSpeed: truck.speed.kph,
+//   //   currentFuel: truck.fuel.value,
+//   //   totalFuel: truck.fuel.capacity,
+
+//   //   hazardLight: truck.lights.hazard?.enabled,
+//   //   cruizeControl: truck.cruiseControl.enabled,
+//   //   highLight: truck.lights.beamHigh.enabled,
+//   //   engineBreak: truck.brakes.motor.enabled,
+
+//   //   differential: truck.differential.lock?.enabled,
+//   //   light: truck.lights.beacon.enabled,
+//   //   retarderStatus: truck.brakes.retarder.steps,
+
+//   //   currentMaxSpeed: navigation.speedLimit.kph,
+//   // };
+
+//   io.emit('speed', truck.speed.kph);
+
+//   io.emit('speed_limit', speedLimit);
+// });
+
 app.get('/', (req, res) => {
-  res.send({ message: 'Home' });
+  res.send({ message: 'Wellcome to temeltry ets api' });
 });
 
 serverHttp.listen(3001, () => {
